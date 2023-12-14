f49129a81d9c2f13cbd5914fd9ab68627c9935bb
richalsthomask <richalsthomask@gmail.com>
2023-12-14 15:34:58 +0530

    configing for deployment

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  assetPrefix: "./",
+  output: "export",
 };
 
 export default nextConfig;
